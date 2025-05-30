d4807a86b3d5764cbca64a92c39e988c550b664a
Asafiz <asaf.izhak@gmail.com>
2025-05-30 16:47:02 +0300

    5

--- Asafiz-Ai-Simple.py
+++ Asafiz-Ai-Simple.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-# Get configuration from environment variables (GitHub Actions secrets)
+# Get configuration from environment variables (GitHub Actions secrets) !
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
 AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
